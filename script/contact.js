0471acf3899d2e7b9c09e0c0ff824450db83f686
affe9911 <affewidlof@hotmail.com>
2017-02-20 12:03:47 +0100

    Add files via upload

---
+++ script/contact.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openDiv() {
+    document.getElementById("Div").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeDiv() {
+    document.getElementById("Div").style.height = "0%";
+}
